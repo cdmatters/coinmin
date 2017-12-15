d70d4f6a4757be6ad2d36e6d1c478300034f3c53
Eric Hambro <eric.hambro@gmail.com>
2017-12-15 23:09:41 +0000

    went the whole hog and added a logo

--- coinmin.sh
+++ coinmin.sh
@@ -1,44 +1,55 @@
-#usr/bin/bash
-
+#/usr/bin/bash
 
 
 
 function coinmin(){
 
+  YELLOW=$(tput setaf 3)
+  GREEN=$(tput setaf 2)
+  RED=$(tput setaf 1)
+  GREY=$(tput bold)$(tput setaf 0)
+  RESET=$(tput sgr0 )
+  
+  echo $YELLOW"
+          (_)               (_)
+  ___ ___  _ _ __  _ __ ___  _ _ __
+ / __/ _ \\| | '_ \\| '_ \` _ \\| | '_ \\ 
+| (_| (_) | | | | | | | | | | | | | |
+ \\___\\___/|_|_| |_|_| |_| |_|_|_| |_|
+ "$RESET
+
+
   LIMIT="10"
   API="https://api.coinmarketcap.com/v1/ticker/?"
   FIELDS='rank\|symbol\|price_usd\|percent\|cap'
 
-  yellow=$(tput setaf 3)
-  green=$(tput setaf 2)
-  red=$(tput setaf 1)
-  grey=$(tput bold)$(tput setaf 0)
-  reset=$(tput sgr0 )
-  
+  BAR1="|------------------------------------------------------------------------------|\n"
+  BAR2="|------|------------|--------------|----------|----------|----------|----------|\n"
+
+
+  # Okay so I went HAM on the one liner...
   curl -s  $API"limit="$LIMIT |\
     grep $FIELDS  |\
     sed 's/ *"symbol": "\(.*\)"/- \1/' |\
     sed 's/^.*\": \"\(.*\)\"/\1/'   |\
     xargs echo | awk '{ split($0, coin, /- /);
-        
-      printf "|------------------------------------------------------------------------------|\n"
-      printf "| Rank | Coin       | Price (USD)  | 24h +/-  | 1h +/-   | 7d +/-   | MktCap $ |\n"
-      printf "|------------------------------------------------------------------------------|\n"
+      printf "'$BAR1'"
+      printf "| Rank | Coin       | Price (USD)  "
+      printf "| 24h +/-  | 1h +/-   | 7d +/-   | MktCap $ |\n"
+      printf "'$BAR1'"
       for (i in coin){
           split(coin[i], c, /, /)
           if (c[2] != ""){
-              printf "| %-4s | %-10s | %-12.4f | +%-7.2f | +%-7.2f | +%-7.2f | %-8.3f |\n", 
+              printf "| %-5s| %-11s| %-13.4f| +%-8.2f| +%-8.2f| +%-8.2f| %-9.3f|\n", 
               c[2], c[1], c[3], c[6], c[5], c[7], (c[4]/1000000000)
-              printf "|------|------------|--------------|----------|----------|----------|----------|\n"
-        }}       
-      }' |\
-    sed -E  "s/([A-Z0-9]+[a-z]+)/"$yellow"\1"$reset"/g" |\
-    sed -E  "s/(\+\/\-)/"$yellow"\1"$reset"/g" |\
-    sed -E  "s/(\(USD\))/"$yellow"\1"$reset"/g" |\
-    sed -E  "s/(\+\-[0-9.]+)/"$red"\1"$reset"/g" |\
-    sed -E  "s/(\+[0-9.]+)/"$green"\1"$reset"/g" |\
-    sed "s/\|/"$grey"\|"$reset"/g" |\
-    sed "s/--\(-*\)/"$grey"--\1"$reset"/g" 
+              printf "'$BAR2'"
+          }
+      }      
+    }' |\
+    sed -E  "s/([A-Z0-9]+[a-z]+)|(\+\/\-)|(\(USD\))/"$YELLOW"\1\2\3"$RESET"/g"|\
+    sed -E  "s/\+(\-[0-9.]+)/"$RED"\1 "$RESET"/g" |\
+    sed -E  "s/(\+[0-9.]+)/"$GREEN"\1"$RESET"/g" |\
+    sed -E  "s/(\|)|(--+)/"$GREY"\1\2"$RESET"/g"
 
 }
 
